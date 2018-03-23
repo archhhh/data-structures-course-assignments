844eb4fe7e3ccc614d06dda3a875857d3038e3d1
uni20162017 <artychen13@unist.ac.kr>
2018-03-23 15:47:30 +0900

    postfix

--- assignment1/assignment1.cpp
+++ assignment1/assignment1.cpp
@@ -8,31 +8,105 @@
 
 
 // include header filers here
-
+#include "assignment1.h"
+#include <stdexcept>
+#include "LinkedStack.h"
 
 using namespace std;
 
-
 int getPriority(Token token) {
-
-
+    switch(token.getOperator())
+    {
+      case 0: return 3;
+              break;
+      case 1: return 3;
+              break;
+      case 2: return 2;
+              break;
+      case 3: return 2;
+              break;
+      case 4: return 0;
+              break;
+      default:
+              throw runtime_error("Wrong operator");
+    }
 }
-
 
 int getInStackPriority(Token token) {
-
-
+    switch(token.getOperator())
+    {
+      case 0: return 3;
+              break;
+      case 1: return 3;
+              break;
+      case 2: return 2;
+              break;
+      case 3: return 2;
+              break;
+      case 4: return 8;
+              break;
+      default:
+              throw runtime_error("Wrong operator");
+    }
 }
-
-
 vector<Token> postfix(const vector<Token>& expression) {
-
-
+    vector <Token> result;
+    LinkedStack<Token> stack;
+    for(int i = 0; i < expression.size(); i++)
+    {
+        if(expression[i].isConstant())
+        {
+          result.push_back(expression[i]);
+        }else if(expression[i].getOperator() == 5)
+        {
+          for(;stack.top().getOperator() != '('; stack.pop())(
+          {
+            result.push_back(stack.top());
+          }
+          stack.pop();
+        }else
+        {
+          for(;getInStackPriority(stack.top()) <= getPriority(expression[i]); stack.pop())
+          {
+            result.push_back(stack.top());
+          }
+          stack.push(expression[i]);
+        }
+    }
+    while(!stack.empty())
+    {
+      result.push_back(stack.top());
+      stack.pop();
+    }
+    return result;
 }
-
-
 int eval(const vector<Token>& expression) {
-
-
+    LinkedStack <Token> stack;
+    for(int i = 0; i < expression.size(); i++)
+    {
+      if(expression[i].isConstant())
+      {
+        stack.push(expression[i]);
+      }else{
+        Token a = stack.top();
+        stack.pop;
+        Token b = stack.top();
+        stack.pop;
+        Token c;
+        c._type = 0;
+        switch(expression[i].getOperator())
+        {
+          case 0:c._constant = a._constant + b._constant;
+                 break;
+          case 1:c._constant = a._constant - b._constant;
+                 break;
+          case 2:c._constant = a._constant * b._constant;
+                 break;
+          case 3:c._constant = a._constant / b._constant;
+                 break;
+        }
+        stack.push(c);
+      }
+    }
+    return stack.top().getConstant();
 }
-
