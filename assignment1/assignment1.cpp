ee3a813db6257356bc60c6ad9576502eebfdceba
uni20162017 <artychen13@unist.ac.kr>
2018-03-25 00:12:56 +0900

    1

--- assignment1/assignment1.cpp
+++ assignment1/assignment1.cpp
@@ -1,4 +1,3 @@
-// Artyom Chen 20162017 artychen13@unist.ac.kr
 //
 // assignment1.cpp (Version 1.0)
 //
@@ -6,8 +5,27 @@
 // Moreover, please describe the implementation of your functions here.
 // You will have to submit this file.
 //
-
-
+/*
+  Artyom Chen 20162017 artychen13@unist.ac.kr
+  Function getPriority uses switch operator to check the type of the operator.
+  Returns the corresponding values representing the incoming priority of the operator.
+  The less the value, the higher the priority. If no type corresponds, throws an exception.
+  Function getInStackPriority has the same implementation as getPriority, but returns instack priority values.
+  Function postfix converts already tokenized infix expression to postfix expression.
+  Vector result contains the postfix expression and stack is used to contain operators. If the operand is
+  encountered, simply add it to the resultant vector that represents the postfix expression. If
+  the operator follows, add all the operators with higher priority from the stack to the vector.
+  If the operator is closing bracket, add all the operators from the stack until the opening bracket
+  is met. After all the operations, add all the remaining operators that are left in the stack to the vector.
+  This function throws the following exceptions. If there is opening bracket and the closing bracket is
+  missing, it throws an exception. If there is closing bracket and the opening bracket is missing,
+  it throws an exception.
+  Function eval evaluates postfix expression. The function is implemented with the help of stack.
+  It traverses through the expression, if the operator is encountered, adds it to the stack, otherwise, takes two operators from the stack and make
+  the corresponding evaluation. The resultant value is put back to the stack. By the end of the iteration
+  the resultant value is value in the stack. Function throws exceptions, if there are no operators left
+  to use the operand on, or when there are no operands left to perform the expression on the operands.
+*/
 // include header filers here
 #include "assignment1.h"
 #include <stdexcept>
