db9e3029e125ed49f31fd462e962c89144da5d6c
uni20162017 <artychen13@unist.ac.kr>
2018-04-15 13:56:48 +0900

    1

--- assignment2/LinkedBinaryTree.h
+++ assignment2/LinkedBinaryTree.h
@@ -7,6 +7,36 @@
 //
 // Artyom Chen, 20162017, artychen13@gmail.com
 //
+/*
+  Class LinkedBinaryTree describes binary tree stucture with nodes. Every node is decribed through the struct
+  Node, which is a protected member of LinkedBinaryTree. Node has element of type Elem, pointer to the left and
+  right children, parent. Also there is constructor that initializes a node. Access to the nodes is provided
+  through Position ADT, which is described through public class Position. Class contains private member Node * v
+  which is pointer to node. Besides, it has several member functions: constructor, which initializes node;
+  overloaded operator *, which returns element of Node; left, right, parent, which return Position of left,
+  right children or parent respectively; isRoot, which returns true, if the Position object points to root,
+  false otherwise; isExternal, which returns true, if the Position object points to external Node, false otherwise;
+  isNull, which returns True, if the Position object points to Null pointer, false otherwise.
+  To grant access to the node to the class LinkedBinaryTree, class is made as friend of Position class.
+  In addition, class LinkedBinaryTree has private members Node *root, which points to root of the binary tree,
+  and int n, which stores the number of nodes in the LinkedBinaryTree object. Also, class has the following functions:
+  constructor, which initializes an empty tree; size, which returns n; empty, which returns True, if tree is empty, false otherwise;
+  root, which returns Position object pointing to root; positions, which returns List ADT of Position objects, which stores
+  Position objects of all nodes. List ADT is provided by <list> STL, and objects are stored in preorder traversal; addRoot, which
+  creates a root Node. Throws exception, if tree is not empty; preorder is utility to help positions function to implement preorder
+  traversal; expandExternal, given Position object pointing to external node, adds left and right children to it, throws exception if object points to NULL, or
+  not external; removeAboveExternal, given Position object pointing to external node, delets the node pointed by object and its parent. Grandparent
+  then points to the left or right child, depending on on which site the deleted parent was. Function returns a new child of grandparent.
+  Throws exception, if the given object points to NULL, not external node, or root node; height, returns the height of the tree.
+  If tree is empty, throws exception, otherwise it goes through positions of all nodes. If the node is external, then checks the depth of
+  the node. Returns maximum depth of all external nodes; attachLeftSubtree, given Position object and LinkedBinaryTree object, attachs LinkedBinaryTree
+  object as left subtree to the tree at Position object. Throws exception, if Position object has left child. Does not do anything,
+  if the Position object points to NULL. Cleans LinkedBinaryTree object; attachRightSubtree is implemented as attachLeftSubtree;
+  removeSubtree, given Position object pointing to the root of subtree, deletes the subtree. If Position object points to NULL node, does nothing,
+  otherwise, deletes the tree one by one using recursion, where base case is external node.
+
+
+*/
 #ifndef LINKED_BINARY_TREE_H
 #define LINKED_BINARY_TREE_H
 
@@ -26,7 +56,7 @@ protected:
     Node():
     elem(), par(NULL), left(NULL), right(NULL)
     { }
-    };
+  };
 public:
   class Position{
     private:
@@ -91,9 +121,7 @@ bool LinkedBinaryTree<Elem>::empty() const
 template <typename Elem>
 typename LinkedBinaryTree<Elem>::Position LinkedBinaryTree<Elem>::root() const
 {
-  if(_root)
-    return Position(_root);
-  throw runtime_error("Tree is empty.");
+  return Position(_root);
 }
 template <typename Elem>
 typename LinkedBinaryTree<Elem>::PositionList LinkedBinaryTree<Elem>::positions() const
@@ -129,9 +157,9 @@ void LinkedBinaryTree<Elem>::expandExternal(const Position &p)
     throw runtime_error("The node p is not external.");
   Node*v = p.v;
   v->left = new Node;
-  v->left->par = p;
+  v->left->par = p.v;
   v->right = new Node;
-  v->right->par = p;
+  v->right->par = p.v;
   n+=2;
 }
 template <typename Elem>
@@ -192,37 +220,48 @@ int LinkedBinaryTree<Elem>::height() const
 template <typename Elem>
 void LinkedBinaryTree<Elem>::attachLeftSubtree(const Position&p, LinkedBinaryTree&subtree)
 {
-  if(!p.left().isNull())
-    throw runtime_error("Node p has left child.");
-  (p.v)->left = subtree.root().v;
-  subtree.root().v->par = p.v;
-  n += subtree.n;
+  if(!p.isNull())
+  {
+    if(!p.left().isNull())
+      throw runtime_error("Node p has left child.");
+    (p.v)->left = subtree.root().v;
+    (p.v)->left->par = p.v;
+    n += subtree.n;
+    subtree._root = NULL;
+    subtree.n = 0;
+  }
 }
 template <typename Elem>
 void LinkedBinaryTree<Elem>::attachRightSubtree(const Position&p, LinkedBinaryTree&subtree)
 {
-  if(!p.right().isNull())
-    throw runtime_error("Node p has right child.");
-  (p.v)->right = subtree.root().v;
-  subtree.root().v->par = p.v;
-  n += subtree.n;
+  if(!p.isNull())
+  {
+    if(!p.right().isNull())
+      throw runtime_error("Node p has right child.");
+    (p.v)->right = subtree.root().v;
+    (p.v)->right->par = p.v;
+    n += subtree.n;
+    subtree._root = NULL;
+    subtree.n = 0;
+  }
 }
 template <typename Elem>
 void LinkedBinaryTree<Elem>::removeSubtree(const Position &p)
 {
-  if(p.isNull())
-    throw runtime_error("Empty subtree.");
-  if(p.isExternal())
+  if(!p.isNull())
   {
-    delete p.v;
-  }else
-  {
-    Position l = p.left();
-    Position r = p.right();
-    delete p.v;
-    removeSubtree(l);
-    removeSubtree(r);
+    if(p.isExternal())
+    {
+      delete p.v;
+    }else
+    {
+      Position l = p.left();
+      Position r = p.right();
+      delete p.v;
+      removeSubtree(l);
+      removeSubtree(r);
+    }
+    n--;
   }
-  n--;
 }
 #endif
--- assignment2/assignment2.cpp
+++ assignment2/assignment2.cpp
@@ -4,7 +4,35 @@
 // Please write your name, your student ID, and your email here.
 // Moreover, please describe the implementation of your functions here.
 // You will have to submit this file.
-//
+// Artyom Chen, 20162017, artychen13@gmail.com
+/*
+  parsePostfixExpression: given postorder expression, stores it in LinkedBinaryTree in postorder manner. Function is implemented, using stack of
+  LinkedBinaryTree objects. As we go through the list, we store each token in a separate LinkedBinaryTree. If the token is operand, we simply
+  put the tree into stack. If the token is operator, we take trees from the stack and append them to the current tree. If there are no trees available,
+  throws exception, otherwise the resultant tree is stored again in stack.
+  print_inorder: implemented, using utility function print_inorder_recursive, which prints nodes of LinkedBinaryTree object in inorder manner, which
+  results in infix expression output. print_inorder_recursive takes Position object, initally pointing to root. If it reaches external Node, it simply
+  outputs the value of the node, otherwise, "( " is printed, then the function calls itself recursively, passing Position object's left child, which
+  allows to traverse to the left first. After it returns, middle value of the node passed to the calling function is printed, then using recursion,
+  the function traverses to the right. Upon return, closing bracket is printed.
+  print_postorder: prints the values of the tree in postorder manner. The function is implemented iteratively, using two stacks of Position objects,
+  st and res, where res will store the resultant expression. The root node is pushed into st stack. Then, until st stack is empty, we take node from the st stack and
+  push it to the res stack, while storing the children of current node in st stack. After that, the values are poped from res stack and printed, until res stack is
+  empty.
+  findMinimumDepth: given tree and symbol sym, finds the minimum depth of sym in the tree. If there is single node, containing sym, then it returns depth
+  of that node. To implement the function, we simply go through the positions of all nodes. Variable int h contains minimum depth found so far. Notice, it
+  is impossible for any node to have tree.height()+1 depth, since it is larger than height of the tree. If current position stores sym, function finds its depth,
+  as it was implemented in LinkedBinaryTree.height() function, then the depth is compared to h, and if it is smaller, then assign current depth to h.
+  If variable h is unchanged after iteration, that means that no node contains such symbol. Then, we simply return -1. Otherwise, h is returned.
+  substitute: given tree, symbol variable and symbol constant, function changes value of each node containing symbol variable to symbol constant.
+  Function iterates through positions of all nodes, and if current node's value is equal to variable, then the value is changed to constant.
+  simplify_subtree: given tree, containing expression, function simplifes the tree, evaluating all the subtrees, whose external nodes are constant values.
+  The implementation combines both of parsePostfixExpression and print_postorder. First, tree is traversed in postorder manner and all the nodes
+  are stored as Position objects in PositionList. This PositionList contains expression in postorder notation. Then as in parsePostfixExpression,
+  function iterates through the list, but if the operator is encountered and two operands poped from the stack are constants, then instead of
+  attaching the trees, expression is evaluated and the value stored in the tree tr and then pushed to the stack. If there is division by 0 , functions
+  throws exception.
+   */
 #include <iostream>
 #include <stdexcept>
 #include <list>
