331afe796756b4dc25269e0b8a5026c151902c89
uni20162017 <artychen13@unist.ac.kr>
2018-05-12 21:36:08 +0900

    1

--- assignment3/ArrayCompleteTree.h
+++ assignment3/ArrayCompleteTree.h
@@ -29,46 +29,263 @@ using namespace std;
 template<typename E>
 class ArrayCompleteTree {
 public:
-
   /*
    * Locator
    */
   class Locator {
+  public:
+    Locator(int _i){
+      i = _i;
+    }
+  private:
     int i;   // the index of the location-aware entry in v
-
-
+    friend class ArrayCompleteTree;
   };
-
   /*
    * Location Aware Entry
    */
-  class LocationAwareEntry {
+  class LocationAwareEntry{
+  public:
+    LocationAwareEntry(){
+      e = E();
+      loc = NULL;
+    }
+    LocationAwareEntry(E _e, int i){
+      e = _e;
+      loc = new Locator(i);
+    }
+    void clear();
+  private:
     E e;             // the element
     Locator *loc;    // a pointer to the locator of this entry
-
-
+    friend class ArrayCompleteTree;
   };
-
   /*
    * Position
    */
   class Position {
+  public:
+    Position(){
+      tree = NULL;
+      loc = NULL;
+    }
+    Position(const ArrayCompleteTree * _tree, Locator * _loc){
+      tree = _tree;
+      loc = _loc;
+    }
+    bool operator==(const Position &p) const;
+    E& operator*();
+  private:
     const ArrayCompleteTree* tree;     // a pointer to the ArrayCompleteTree object that this position object works for.
     Locator *loc;                       // a pointer to the locator object of the entry pointed by this Position object.
-
-
+    friend class ArrayCompleteTree;
   };
-
-
-
-
+  ArrayCompleteTree(int _vsize = 10);
+  ArrayCompleteTree(const ArrayCompleteTree& t);
+  ~ArrayCompleteTree();
+  int size() const;
+  Position left(const Position &p) const;
+  Position right(const Position &p) const;
+  Position parent(const Position &p) const;
+  bool hasLeft(const Position &p) const;
+  bool hasRight(const Position &p) const;
+  bool isRoot(const Position &p) const;
+  Position root() const;
+  Position last() const;
+  void addLast(const E& e);
+  void removeLast();
+  void swap(const Position &p, const Position &q);
+  void cleanup();
 private:
-
   LocationAwareEntry* v;   // the array of location-aware entries
   int vsize;               // the size of v
   int n;                   // the number of used entries in v
-
 };
-
-
+template<typename E>
+void ArrayCompleteTree<E>::LocationAwareEntry::clear(){
+  delete loc;
+  loc = NULL;
+}
+template<typename E>
+bool ArrayCompleteTree<E>::Position::operator==(const Position &p) const{
+  return (tree == p.tree) && (loc == p.loc);
+}
+template<typename E>
+E& ArrayCompleteTree<E>::Position::operator*(){
+  if(!tree)
+    throw runtime_error("Wrong position.");
+  if(!loc)
+    throw runtime_error("Wrong position.");
+  if(loc->i < 0 || loc->i >= tree->n)
+    throw runtime_error("Wrong position.");
+  if(tree->v[loc->i].loc != loc)
+      throw runtime_error("Wrong position.");
+  return tree->v[loc->i].e;
+}
+template<typename E>
+ArrayCompleteTree<E>::ArrayCompleteTree(int _vsize = 10){
+  v = new LocationAwareEntry[10];
+  vsize = _vsize;
+  n = 0;
+}
+template<typename E>
+ArrayCompleteTree<E>::ArrayCompleteTree(const ArrayCompleteTree& t){
+  _v = new LocationAwareEntry[t.vsize];
+  vsize = t.vsize;
+  n = t.n;
+  for(int i = 0; i < n;i++)
+    _v[i] = LocationAwareEntry((t.v)[i].e, (t.v)[i].loc->i);
+  ~ArrayCompleteTree();
+  v = _v;
+}
+template<typename E>
+ArrayCompleteTree<E>::~ArrayCompleteTree(){
+  for(int i = 0; i < n; i++)
+    v[i].clear();
+  delete [] v;
+}
+template<typename E>
+int ArrayCompleteTree<E>::size() const{
+  return n;
+}
+template<typename E>
+Position ArrayCompleteTree<E>::left(const Position&p) const{
+  if(!hasLeft(p))
+    throw runtime_error("No left child.");
+  return Position(this, v[2*((p.loc)->i)+1].loc);
+}
+template<typename E>
+Position ArrayCompleteTree<E>::right(const Position&p) const{
+  if(!hasRight(p))
+    throw runtime_error("No right child.");
+  return Position(this, v[2*((p.loc)->i)+2].loc);
+}
+template<typename E>
+Position ArrayCompleteTree<E>::parent(const Position&p) const{
+  if(isRoot(p))
+    throw runtime_error("No parent.");
+  return Position(this, v[(((p.loc)->i)+1)/2].loc);
+}
+template<typename E>
+bool ArrayCompleteTree<E>::hasLeft(const Position &p) const{
+  if(!p.tree)
+    throw runtime_error("Wrong position.");
+  if(!p.loc)
+    throw runtime_error("Wrong position.");
+  if(p.tree != this)
+    throw runtime_error("Wrong position.");
+  if((p.loc)->i < 0 || (p.loc)->i >= n)
+    throw runtime_error("Wrong position.");
+  if(p.tree->v[i].loc != p.loc)
+    throw runtime_error("Wrong position.");
+  if((2*((p.loc)->i)+1) >= n)
+    return false;
+  return true;
+}
+template<typename E>
+bool ArrayCompleteTree<E>::hasRight(const Position &p) const{
+  if(!p.tree)
+    throw runtime_error("Wrong position.");
+  if(!p.loc)
+    throw runtime_error("Wrong position.");
+  if(p.tree != this)
+    throw runtime_error("Wrong position.");
+  if((p.loc)->i < 0 || (p.loc)->i >= n)
+    throw runtime_error("Wrong position.");
+  if(p.tree->v[i].loc != p.loc)
+    throw runtime_error("Wrong position.");
+  if((2*((p.loc)->i)+2) >= n)
+    return false;
+  return true;
+}
+template<typename E>
+bool ArrayCompleteTree<E>::isRoot(const Position &p) const{
+  if(!p.tree)
+    throw runtime_error("Wrong position.");
+  if(!p.loc)
+    throw runtime_error("Wrong position.");
+  if(p.tree != this)
+    throw runtime_error("Wrong position.");
+  if((p.loc)->i < 0 || (p.loc)->i >= n)
+      throw runtime_error("Wrong position.");
+  if(p.tree->v[i].loc != p.loc)
+      throw runtime_error("Wrong position.");
+  if((p.loc->i) != 0)
+      return false;
+  return true;
+}
+template<typename E>
+Position ArrayCompleteTree<E>::root() const{
+  if(n == 0)
+    throw runtime_error("Tree is empty.");
+  return Position(this, v[0].loc);
+}
+template<typename E>
+Position ArrayCompleteTree<E>::last() const{
+  if(n == 0)
+    throw runtime_error("Tree is empty.");
+  return Position(this, v[n-1].loc);
+}
+template<typename E>
+void ArrayCompleteTree<E>::addLast(const E& e){
+  if(n < vsize)
+  {
+    v[n] = LocationAwareEntry(e, n);
+  }else
+  {
+    _v = new LocationAwareEntry[2*vsize];
+    for(int i = 0; i < n; i++)
+      _v[i] = v[i];
+    _v[n] = = LocationAwareEntry(e, n);
+    delete [] v;
+    v = _v;
+    vsize = 2*vsize;
+  }
+  n++;
+}
+template<typename E>
+void ArrayCompleteTree<E>::removeLast(const E& e){
+  if(n == 0)
+    throw runtime_error("Empty tree.");
+  n--;
+}
+template<typename E>
+void ArrayCompleteTree<E>::swap(const Position& p, const Position &q){
+  if(!p.tree || !q.tree)
+  if(p.tree != q.tree)
+    throw runtime_error("Wrong position.");
+  if(p.tree != this)
+    throw runtime_error("Wrong position.");
+  if(!p.loc || !q.loc)
+    throw runtime_error("Wrong position.");
+  if((p.loc)->i < 0 || (p.loc)->i >= n ||(q.loc)->i < 0 || (q.loc)->i >= n)
+      throw runtime_error("Wrong position.");
+  if(p.tree->v[i].loc != p.loc || q.tree->v[i].loc != q.loc)
+      throw runtime_error("Wrong position.");
+  E temp_e = *p;
+  *p = *q;
+  *q = temp_e;
+  Locator * temp_loc = p.loc;
+  p.loc = q.loc;
+  q.loc = temp_loc;
+  int temp_ind = p.loc->i;
+  p.loc->i = q.loc->i;
+  q.loc->i = p.loc->i;
+  v[p.loc->i].loc = p.loc;
+  v[q.loc->i].loc = q.loc;
+}
+template<typename E>
+void ArrayCompleteTree<E>::cleanup(){
+  if(n < vsize)
+  {
+    _v = new LocationAwareEntry[n];
+    for(int i = 0; i < n; i++)
+      _v[i] = v[i];
+    for(int i = n; i < vsize; i++)
+      v[i].clear;
+    delete [] v;
+    v = _v;
+    vsize = n;
+  }
+}
 #endif //ASSIGNMENT3_ARRAYCOMPLETETREE_H
--- assignment3/HeapPriorityQueue.h
+++ assignment3/HeapPriorityQueue.h
@@ -28,8 +28,6 @@ class LessThanComparator {
 public:
   bool operator()(const T& a, const T& b) const { return a < b; }
 };
-
-
 /*
  * A heap-based priority queue based on ArrayCompleteTree.
  * See Section 8.3.4 in the textbook for more information.
@@ -38,17 +36,86 @@ public:
  */
 template<typename E, typename C>
 class HeapPriorityQueue {
-
-
-
-
-
+public:
+  HeapPriorityQueue();
+  HeapPriorityQueue(const HeapPriorityQueue &q);
+  int size() const;
+  bool empty() const;
+  Position insert(const E&e);
+  const E& min();
+  void removeMin();
+  void remove(const Position &u);
+  Position replace(const Position &p, const E& e);
 private:
 
   ArrayCompleteTree<E> T;  // the array-based complete tree
   C isLess;                // the less-than comparator
 
+  void bubbleUp(const Position& u)
+  void bubbleDown(const Position &u)
+  typedef typename ArrayCompleteTree<E>::Position Position
 };
-
-
+template<typename E, typename C>
+HeapPriorityQueue<E,C>::HeapPriorityQueue():T(), isLess(){
+}
+template<typename E, typename C>
+HeapPriorityQueue<E,C>::HeapPriorityQueue(const HeapPriorityQueue&q):T(q), isLess(){
+}
+template<typename E, typename C>
+int HeapPriorityQueue<E,C>::size() const{
+  return T.size();
+}
+template<typename E, typename C>
+bool HeapPriorityQueue<E,C>::empty() const{
+  return T.size() == 0;
+}
+template<typename E, typename C>
+Position HeapPriorityQueue<E,C>:: insert(const E&e){
+  T.addLast(e);
+  bubbleUp(T.last());
+}
+template<typename E, typename C>
+const E& HeapPriorityQueue<E,C>::min(){
+  return *T.root();
+}
+template<typename E, typename C>
+void HeapPriorityQueue<E,C>::removeMin(){
+  remove(T.root());
+}
+template<typename E, typename C>
+void HeapPriorityQueue<E,C>::remove(const Position &u){
+  T.swap(u, T.last());
+  T.removeLast();
+  bubbleDown(u);
+}
+template<typename E, typename C>
+Position HeapPriorityQueue<E,C>::replace(const Position &p, const E& e){
+  if(e < *p)
+  {
+    *p = e;
+    bubbleUp(p);
+  }else if(e > *p)
+  {
+    *p = e;
+    bubbleDown(p);
+  }
+}
+template<typename E, typename C>
+void HeapPriorityQueue<E,C>::bubbleUp(const Position &u){
+  while(!T.isRoot(u) && isLess(*u, *T.parent(u)))
+    T.swap(u, T.parent(u));
+}
+template<typename E, typename C>
+void HeapPriorityQueue<E,C>::bubbleDown(const Position &u){
+  while(T.hasLeft(u))
+  {
+    Position v = T.left(u);
+    if(T.hasRight(u) && isLess(*T.right(u), *v))
+      v = T.right(u);
+    if(isLess(*u, *v))
+      T.swap(u,v);
+    else
+      break;
+  }
+}
 #endif //ASSIGNMENT3_HEAPPRIORITYQUEUE_H
